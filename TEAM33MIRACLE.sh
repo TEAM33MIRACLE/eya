4198e8d60a7fb1f60d2e4ca89e9cadc5365d5551
TEAM33MIRACLE <39024897+TEAM33MIRACLE@users.noreply.github.com>
2018-05-06 19:54:04 +0700

    Add files via upload

---
+++ TEAM33MIRACLE.sh
@@ -0,0 +1,92 @@
+#!/system/xbin/bash
+clear
+toilet -f slant -F gay "LITESCmaster"
+sleep 1
+echo "\033[33;1m versi 1.0"
+sleep 1
+echo "\033[33;1m kumpulan tools scdownloader n maker"
+sleep 1
+echo "\033[34;1m thanks to INDONESIA SECURITY LITE"
+sleep 1
+echo "\033[35;1m thanks for author htmd, LITESPAM, automake sc deface"
+sleep 1
+echo "\033[36;1m LITESCmaster by AQuamarine"
+sleep 1
+echo "\033[36;1m tools nya :"
+echo "1 Htmd"
+echo "2 sc maker"
+echo "3 tentang tools ini"
+echo "0 out"
+echo "\033[33;1m pilih mastah :"
+read mmm
+if [ $mmm = 1 ] || [ $mmm = 1 ]
+then
+clear
+toilet -f mono12 -F gay "HTMD"
+echo "\033[35;1m recomend ambil sc di poc FCKeditoro"
+echo "\033[35;1m contoh ~ https://website.com/files/sc.html"
+echo "\033[35;1m dork inurl:advert_detail.php?id="
+echo "\033[33;1m"
+python2 2.py
+fi
+
+if [ $mmm = 2 ] || [ $mmm = 1 ]
+then
+clear
+toilet -f slant -F gay "SCmaker"
+echo "\033[35;1m versi satu"
+echo "\033[36;1m"
+python2 3.py
+fi
+
+if [ $mmm = 3 ] || [ $mmm = 1 ]
+then
+clear
+toilet -f slant -F gay "LITESCmaster"
+echo "\033[31;1m nama tools : LITESCmaster"
+sleep 1
+echo "\033[32;1m by : AQuamarine sbagai bahan belajar"
+sleep 1
+echo "\033[33;1m versi : V1.0"
+sleep 1
+echo "\033[34;1m team : INDONESIA SECURITY LITE"
+sleep 1
+echo "\033[35;1m ABOUT : maaf tools ini hanya bahan belajar, karena saya ingin lebih mengenal dunia coding"
+echo "\033[36;1m special thanks to :"
+echo "\033[31;1m"
+echo "AUTHOR LITESPAM"
+echo "AUTHOR HTMD"
+echo "AUTHOR SC MAKER leader indonesian termux"
+echo "and all admin and member INDONESIA SECURITY LITE"
+sleep 1
+echo "\033[30;1m tunggu 7 detik"
+sleep 7
+sh LITESC.sh
+fi
+
+if [ $mmm = 0 ] || [ $mmm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 2
+echo "\033[31;1m sampai jumpa mastah"
+fi
+com/files/sc.html"
+echo "\033[35;1m dork inurl:advert_detail.php?id="
+echo "\033[33;1m"
+python2 2.py
+fi
+
+if [ $mmm = 2 ] || [ $mmm = 1 ]
+then
+clear
+toilet -f slant -F gay "SCmaker"
+echo "\033[35;1m versi satu"
+echo "\033[36;1m"
+python2 3.py
+fi
+
+if [ $mmm = 3 ] || [ $mmm = 1 ]
+then
+clear
+toilet -f slant -F gay "LITESCmaster"
+echo "\033[31;1m nama 
